2d0e99eb8c118e730ad451f1aa2f7462074ce8fd
Vivek Rajkumar <vivek.rajkumar@protonmail.com>
2018-06-25 14:37:41 -0400

    Refactor getRoundWinner() using helper functions

--- js/game-logic.js
+++ js/game-logic.js
@@ -20,39 +20,49 @@ let playerOneMoveOneType,
 
 // Check if player input is valid
 function validatePlayer(player) {
-  if (validPlayers.includes(String(player))) {
-    return true;
-  }
-
-  return false;
+  return validPlayers.includes(String(player));
 }
 
 // Check if move type inputs are valid
 function validateMoveTypes(moveOneType, moveTwoType, moveThreeType) {
-
-  if (validMoveTypes.includes(moveOneType)
+  return validMoveTypes.includes(moveOneType)
   && validMoveTypes.includes(moveTwoType)
-  && validMoveTypes.includes(moveThreeType)) {
-    return true;
-  }
-
-  return false;
+  && validMoveTypes.includes(moveThreeType);
 }
 
 // Check if move value inputs are valid
 function validateMoveValues(moveOneValue, moveTwoValue, moveThreeValue) {
-
-  if (moveOneValue >= 1
+  return moveOneValue >= 1
     && moveOneValue <= 99
     && moveTwoValue >= 1
     && moveTwoValue <= 99
     && moveThreeValue >= 1
     && moveThreeValue <= 99
-    && moveOneValue + moveTwoValue + moveThreeValue <= 99) {
-    return true;
-  }
+    && moveOneValue + moveTwoValue + moveThreeValue <= 99;
+}
 
-  return false;
+// Set moves for player one
+function setPlayerOneMoves(moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+  playerOneMoveOneType = moveOneType;
+  playerOneMoveOneValue = moveOneValue;
+
+  playerOneMoveTwoType = moveTwoType;
+  playerOneMoveTwoValue = moveTwoValue;
+
+  playerOneMoveThreeType = moveThreeType;
+  playerOneMoveThreeValue = moveThreeValue;
+}
+
+// Set moves for player two
+function setPlayerTwoMoves(moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+  playerTwoMoveOneType = moveOneType;
+  playerTwoMoveOneValue = moveOneValue;
+
+  playerTwoMoveTwoType = moveTwoType;
+  playerTwoMoveTwoValue = moveTwoValue;
+
+  playerTwoMoveThreeType = moveThreeType;
+  playerTwoMoveThreeValue = moveThreeValue;
 }
 
 // Set values for the move and type variables
@@ -66,208 +76,141 @@ function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoV
   && validateMoveValues(moveOneValue, moveTwoValue, moveThreeValue)) {
 
     if (String(player) === 'Player One') {
-
-      // Set first move for player one
-      playerOneMoveOneType = firstMoveType;
-      playerOneMoveOneValue = moveOneValue;
-
-      // Set second move for player one
-      playerOneMoveTwoType = secondMoveType;
-      playerOneMoveTwoValue = moveTwoValue;
-
-      // Set third move for player one
-      playerOneMoveThreeType = thirdMoveType;
-      playerOneMoveThreeValue = moveThreeValue;
-
+      setPlayerOneMoves(firstMoveType, moveOneValue, secondMoveType, moveTwoValue, thirdMoveType, moveThreeValue);
     } else {
-
-      // Set first move for player two
-      playerTwoMoveOneType = firstMoveType;
-      playerTwoMoveOneValue = moveOneValue;
-
-      // Set second move for player two
-      playerTwoMoveTwoType = secondMoveType;
-      playerTwoMoveTwoValue = moveTwoValue;
-
-      // Set third move for player two
-      playerTwoMoveThreeType = thirdMoveType;
-      playerTwoMoveThreeValue = moveThreeValue;
-
+      setPlayerTwoMoves(firstMoveType, moveOneValue, secondMoveType, moveTwoValue, thirdMoveType, moveThreeValue);
     }
-
   }
-
 }
 
-function getRoundWinner(roundNumber) {
-  switch (roundNumber) {
-    // Determine winner for round 1
-    case 1:
+// Initialize shorthand variables
+let playerOneType, playerOneValue, playerTwoType, playerTwoValue = undefined;
 
-      switch (playerOneMoveOneType) {
+function setShorthandVariables(roundNumber) {
+  if (roundNumber === 1) {
+    [playerOneType, playerOneValue] = [playerOneMoveOneType, playerOneMoveOneValue];
+    [playerTwoType, playerTwoValue] = [playerTwoMoveOneType, playerTwoMoveOneValue];
+  } else if (roundNumber === 2) {
+    [playerOneType, playerOneValue] = [playerOneMoveTwoType, playerOneMoveTwoValue];
+    [playerTwoType, playerTwoValue] = [playerTwoMoveTwoType, playerTwoMoveTwoValue];
+  } else if (roundNumber === 3) {
+    [playerOneType, playerOneValue] = [playerOneMoveThreeType, playerOneMoveThreeValue];
+    [playerTwoType, playerTwoValue] = [playerTwoMoveThreeType, playerTwoMoveThreeValue];
+  } else {
+    throw new Error("Invalid round number supplied");
+    return;
+  }
+}
 
-        case 'rock':
-          if (playerTwoMoveOneType == 'scissors') {
-            return 'Player One';
-          } else if (playerTwoMoveOneType == 'paper') {
-            return 'Player Two';
-          } else if (playerTwoMoveOneValue < playerOneMoveOneValue) {
-            return 'Player One';
-          } else if (playerTwoMoveOneValue > playerOneMoveOneValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveOneValue === playerOneMoveOneValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
+// Determine winner for a given round when player one chooses rock
+function playerOneRock(roundNumber) {
+  setShorthandVariables(roundNumber);
 
-        case 'paper':
-          if (playerTwoMoveOneType == 'rock') {
-            return 'Player One';
-          } else if (playerTwoMoveOneType == 'scissors') {
-            return 'Player Two';
-          } else if (playerTwoMoveOneValue < playerOneMoveOneValue) {
-            return 'Player One';
-          } else if (playerTwoMoveOneValue > playerOneMoveOneValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveOneValue === playerOneMoveOneValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
+  if (playerTwoType === 'scissors') {
+    return 'Player One';
+  } else if (playerTwoType === 'paper') {
+    return 'Player Two';
+  } else if (playerTwoType === 'rock' && playerTwoValue < playerOneValue) {
+    return 'Player One';
+  } else if (playerTwoType === 'rock' && playerTwoValue > playerOneValue) {
+    return 'Player Two';
+  } else if (playerTwoType === 'rock' && playerTwoValue === playerOneValue) {
+    return 'Tie';
+  } else {
+    return null;
+  }
+}
 
-        case 'scissors':
-          if (playerTwoMoveOneType == 'paper') {
-            return 'Player One';
-          } else if (playerTwoMoveOneType == 'rock') {
-            return 'Player Two';
-          } else if (playerTwoMoveOneValue < playerOneMoveOneValue) {
-            return 'Player One';
-          } else if (playerTwoMoveOneValue > playerOneMoveOneValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveOneValue === playerOneMoveOneValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
+// Determine winner for a given round when player one chooses paper
+function playerOnePaper(roundNumber) {
+  setShorthandVariables(roundNumber);
 
-        default:
-          return null;
+  if (playerTwoType === 'rock') {
+    return 'Player One';
+  } else if (playerTwoType === 'scissors') {
+    return 'Player Two';
+  } else if (playerTwoType === 'paper' && playerTwoValue < playerOneValue) {
+    return 'Player One';
+  } else if (playerTwoType === 'paper' && playerTwoValue > playerOneValue) {
+    return 'Player Two';
+  } else if (playerTwoType === 'paper' && playerTwoValue === playerOneValue) {
+    return 'Tie';
+  } else {
+    return null;
+  }
+}
 
-      }
+// Determine winner for a given round when player one chooses scissors
+function playerOneScissors(roundNumber) {
+  setShorthandVariables(roundNumber);
 
-    // Determine winner for round 2
-    case 2:
+  if (playerTwoType === 'paper') {
+    return 'Player One';
+  } else if (playerTwoType === 'rock') {
+    return 'Player Two';
+  } else if (playerTwoType === 'scissors' && playerTwoValue < playerOneValue) {
+    return 'Player One';
+  } else if (playerTwoType === 'scissors' && playerTwoValue > playerOneValue) {
+    return 'Player Two';
+  } else if (playerTwoType === 'scissors' && playerTwoValue === playerOneValue) {
+    return 'Tie';
+  } else {
+    return null;
+  }
+}
 
-      switch (playerOneMoveTwoType) {
-
-        case 'rock':
-          if (playerTwoMoveTwoType == 'scissors') {
-            return 'Player One';
-          } else if (playerTwoMoveTwoType == 'paper') {
-            return 'Player Two';
-          } else if (playerTwoMoveTwoValue < playerOneMoveTwoValue) {
-            return 'Player One';
-          } else if (playerTwoMoveTwoValue > playerOneMoveTwoValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveTwoValue === playerOneMoveOneValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
-
-        case 'paper':
-          if (playerTwoMoveTwoType == 'rock') {
-            return 'Player One';
-          } else if (playerTwoMoveTwoType == 'scissors') {
-            return 'Player Two';
-          } else if (playerTwoMoveTwoValue < playerOneMoveTwoValue) {
-            return 'Player One';
-          } else if (playerTwoMoveTwoValue > playerOneMoveTwoValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveTwoValue === playerOneMoveOneValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
-
-        case 'scissors':
-          if (playerTwoMoveTwoType == 'paper') {
-            return 'Player One';
-          } else if (playerTwoMoveTwoType == 'rock') {
-            return 'Player Two';
-          } else if (playerTwoMoveTwoValue < playerOneMoveTwoValue) {
-            return 'Player One';
-          } else if (playerTwoMoveTwoValue > playerOneMoveTwoValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveTwoValue === playerOneMoveOneValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
-
-        default:
-          return null;
-
-      }
-
-    // Determine winner for round 3
-    case 3:
-
-      switch (playerOneMoveThreeType) {
-
-        case 'rock':
-          if (playerTwoMoveThreeType === 'scissors') {
-            return 'Player One';
-          } else if (playerTwoMoveThreeType === 'paper') {
-            return 'Player Two';
-          } else if (playerTwoMoveThreeValue < playerOneMoveThreeValue) {
-            return 'Player One';
-          } else if (playerTwoMoveThreeValue > playerOneMoveThreeValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveThreeValue === playerOneMoveThreeValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
-
-        case 'paper':
-          if (playerTwoMoveThreeType === 'rock') {
-            return 'Player One';
-          } else if (playerTwoMoveThreeType === 'scissors') {
-            return 'Player Two';
-          } else if (playerTwoMoveThreeValue < playerOneMoveThreeValue) {
-            return 'Player One';
-          } else if (playerTwoMoveThreeValue > playerOneMoveThreeValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveThreeValue === playerOneMoveThreeValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
-
-        case 'scissors':
-          if (playerTwoMoveThreeType === 'paper') {
-            return 'Player One';
-          } else if (playerTwoMoveThreeType === 'rock') {
-            return 'Player Two';
-          } else if (playerTwoMoveThreeValue < playerOneMoveThreeValue) {
-            return 'Player One';
-          } else if (playerTwoMoveThreeValue > playerOneMoveThreeValue) {
-            return 'Player Two';
-          } else if (playerTwoMoveThreeValue === playerOneMoveThreeValue) {
-            return 'Tie';
-          } else {
-            return null;
-          }
-
-        default:
-          return null;
-
-      }
-
+// Determine winner of round one
+function getRoundOneWinner() {
+  switch (playerOneMoveOneType) {
+    case 'rock':
+      playerOneRock(1);
+    case 'paper':
+      playerOnePaper(1);
+    case 'scissors':
+      playerOneScissors(1);
     default:
       return null;
+  }
+}
 
+// Determine winner of round two
+function getRoundTwoWinner() {
+  switch (playerOneMoveTwoType) {
+    case 'rock':
+      playerOneRock(2);
+    case 'paper':
+      playerTwoRock(2);
+    case 'scissors':
+      playerTwoScissors(2);
+    default:
+      return null;
+  }
+}
+
+// Determine winner of round three
+function getRoundThreeWinner() {
+  switch (playerOneMoveThreeType) {
+    case 'rock':
+      playerOneRock(3);
+    case 'paper':
+      playerOnePaper(3);
+    case 'scissors':
+      playerOneScissors(3);
+    default:
+      return null;
+  }
+}
+
+// Determine the winner for a given round number
+function getRoundWinner(roundNumber) {
+  switch (roundNumber) {
+    case 1:
+      getRoundOneWinner();
+    case 2:
+      getRoundTwoWinner();
+    case 3:
+      getRoundThreeWinner();
+    default:
+      return null;
   }
 }
